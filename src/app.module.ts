86684818fc7fd049a5f538ee7816799687a7e89d
cesar <c.astorgagonzalez@gmail.com>
2020-11-04 00:14:22 -0300

    config changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Global, Module } from '@nestjs/common';
 import { Configuration } from './config/config.keys';
-import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { MailModule } from './modules/mailer/mail.module';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 
 
 @Global()
 @Module({
-  imports: [ConfigModule,MailModule,HandlebarsAdapter],
+  imports: [ConfigModule.forRoot(),MailModule,HandlebarsAdapter],
 })
 export class AppModule {
   static port: number | string;
